e4e1a68511fec560606f88a4c0982f7c111f651a
Muhammed Karam <muhammedkaram@Muhammeds-MacBook-Air.local>
2023-04-03 23:25:26 +0200

    first commidt

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,15 @@ import './index.css';
 import App from './App';
 import "bootstrap/dist/css/bootstrap.min.css";
 import '@fortawesome/fontawesome-svg-core/styles.css';
-
+import BrowserRouter from 'react-router-dom/BrowserRouter'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+ReactDOM.render((
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <App />
+  </BrowserRouter>
+))
