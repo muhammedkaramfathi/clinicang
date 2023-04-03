c85616df845718d377d83815d8ec4b55281674d8
Muhammed Karam <muhammedkaram@Muhammeds-MacBook-Air.local>
2023-04-04 00:17:04 +0200

    fix2

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import BrowserRouter from 'react-router-dom/BrowserRouter'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter  basename={clinic}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
